754f956744afe82a6d69dcbf9c3d6e1e44ee03b8
Dmitriy Lyuosev <xhaustful@gmail.com>
2023-04-12 16:34:45 +0300

    files_match.py

---
+++ file_match.py
@@ -0,0 +1,23 @@
+import sys
+
+# Check that the user provided a filename as the first argument
+if len(sys.argv) < 2:
+    print("Usage: python script.py filename")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+# Open the file and read its contents into a set
+try:
+    with open(filename, "r") as f:
+        lines = set(f.read().splitlines())
+except FileNotFoundError:
+    print(f"Error: File '{filename}' not found")
+    sys.exit(1)
+
+# Read lines from stdin, and for each one, check if it's in the set of lines from the file
+for i, line in enumerate(sys.stdin):
+    line = line.strip()
+    if line in lines:
+        print(f"{line};{filename}:{i+1}")
+
