7144461480da37ddf97bda297b9012a7b7db0a37
Dmitriy Lyuosev <xhaustful@gmail.com>
2023-05-08 03:38:07 +0300

    Update AST server

--- ast_server/ast_parser.py
+++ ast_server/ast_parser.py
@@ -1,3 +1,29 @@
+import clang
+from flask import jsonify
+import utils
+
+def get_ast(node):
+    location = node.location
+    ast = {
+        "kind": str(node.kind),
+        "spelling": node.spelling,
+        "location": {
+            "line": location.line,
+            "column": location.column,
+            "offset": location.offset,
+            "endLine": node.extent.end.line,
+            "endColumn": node.extent.end.column,
+            "endOffset": node.extent.end.offset
+        },
+        "children": []
+    }
+
+    for child in node.get_children():
+        ast["children"].append(get_ast(child))
+
+    return ast
+
+
 def ast_to_code(node, code):
     # print(node)
 
@@ -27,3 +53,17 @@ def ast_to_code(node, code):
         code += "}\n"
 
     return code
+
+
+def get_translation_unit(path: str):
+    path = utils.find_files_with_name("/home/mg/chromium/src/third_party/blink", path)
+    if not len(path) == 1:
+        return jsonify(path)
+    path = path[0]
+
+    index = clang.cindex.Index.create()
+    translation_unit = index.parse(
+        path=path, args=["-std=c++17", "-x"]
+    )
+
+    return translation_unit
--- ast_server/ast_server.py
+++ ast_server/ast_server.py
@@ -11,35 +11,7 @@ clang.cindex.Config.set_library_file("/home/mg/gh/llvm-project/build/lib/libclan
 app = Flask(__name__)
 CORS(app)
 
-def get_ast(node):
-    # ast = {
-    #     "kind": str(node.kind),
-    #     "spelling": node.spelling,
-    #     "children": [],
-    #     ""
-    # }
-    location = node.location
-    ast = {
-        "kind": str(node.kind),
-        "spelling": node.spelling,
-        "location": {
-            "line": location.line,
-            "column": location.column,
-            "offset": location.offset,
-            "endLine": node.extent.end.line,
-            "endColumn": node.extent.end.column
-        },
-        "children": []
-    }
-
-    for child in node.get_children():
-        ast["children"].append(get_ast(child))
-
-    print(ast)
-    return ast
-
-
-@app.route("/code_from_ast", methods=["POST"])
+@cross_origin()
 def code_from_ast():
     ast = request.get_json()
     if "ast" in ast:
@@ -49,6 +21,7 @@ def code_from_ast():
 
 
 @app.route("/ast", methods=["POST"])
+@cross_origin()
 def ast():
     code = request.data.decode("utf-8")
 
@@ -58,31 +31,45 @@ def ast():
     )
 
     root = translation_unit.cursor
-    ast = get_ast(root)
+    ast = ast_parser.get_ast(root)
 
     return jsonify(ast)
+
+
+include_dirs = [
+    "/home/mg/chromium/src",
+    # "/path/to/your/second/include",
+]
+
+# Convert the include directories into Clang-compatible arguments
+include_args = [f"-I{include_dir}" for include_dir in include_dirs]
 
 
 @app.route("/ast_from_file", methods=["POST"])
+@cross_origin()
 def ast_from_file():
     path = request.data.decode("utf-8")
-    path = utils.find_files_with_name("/home/mg/chromium/src/third_party/blink", path)
+    translation_unit = ast_parser.get_translation_unit(path)
+    try:
+        root = translation_unit.cursor
+        ast = ast_parser.get_ast(root)
+        return jsonify({'contents': ast})
+    except:
+        return translation_unit
 
-    if len(path) > 1:
-        return jsonify(path)
+
+@app.route('/tokenize', methods=['POST'])
+def tokenize():
+    path = request.data.decode("utf-8")
+    tu = ast_parser.get_translation_unit(path)
+    try:
+        tokens = []
+        for token in tu.get_tokens(extent=tu.cursor.extent):
+            tokens.append({'spelling': token.spelling, 'kind': str(token.kind)})
+    except:
+        return tu
     
-    path = path[0]
-
-    index = clang.cindex.Index.create()
-    translation_unit = index.parse(
-        path=path, args=["-std=c++17", "-x", "c++"]
-    )
-
-    root = translation_unit.cursor
-    ast = get_ast(root)
-
-    return jsonify(ast)
-
+    return jsonify(tokens)
 
 @app.route("/src", methods=["POST"])
 @cross_origin()
@@ -90,7 +77,7 @@ def src():
     path = request.data.decode("utf-8")
     path = utils.find_files_with_name("/home/mg/chromium/src/third_party/blink", path)
 
-    if len(path) > 1 or len(path) == 0:
+    if not len(path) == 1:
         return jsonify(path)
     
     path = path[0]
